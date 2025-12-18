4b4b89113ac1873ee2eadbfd84a22acd1987c526
Tuany Farias <tuany.fariass@gmail.com>
2025-12-18 02:21:22 -0300

    add api_key headers

--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -1,11 +1,16 @@
 import { WeatherResponse } from '../dto/WeatherResponse'
+const X_API_KEY = import.meta.env.VITE_X_API_KEY
 
 export async function getWeather(city: string): Promise<WeatherResponse> {
     const url = `http://localhost:5000/api/weather?city=${encodeURIComponent(city)}`
 
-    const response = await fetch(url)
+    const response = await fetch(url, {
+    headers: {
+        'Authorization': X_API_KEY
+    },
+    })
     if (!response.ok) {
-        throw new Error('Erro ao buscar o clima no backend')
+        throw new Error('Algo deu errado na sua requisição!')
     }
 
     const data = await response.json()
